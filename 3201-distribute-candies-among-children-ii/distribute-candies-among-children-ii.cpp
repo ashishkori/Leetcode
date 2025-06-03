cf2af4204834d6374c60de41af19b2a9e98e09ff
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-06-03 17:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (82.35%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     long long distributeCandies(int n, int m) {
@@ -10,5 +11,30 @@ public:
             res += (i % 2 ? -c * val : c * val);
         }
         return res;
+    }
+};
+*/
+
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        auto C2 = [&](long long x) -> long long {
+            return (x >= 2) ? (x * (x - 1) / 2) : 0;
+        };
+
+        long long N = n, L = limit;
+        // Total unconstrained = C(n+2, 2)
+        long long total = (N + 2) * (N + 1) / 2;
+
+        long long x1 = N - L + 1; // for one coord > limit
+        long long t1 = C2(x1);
+
+        long long x2 = N - 2 * L; // for two coord > limit
+        long long t2 = C2(x2);
+
+        long long x3 = N - 3 * L - 1;// for 3 coords > limit
+        long long t3 = C2(x3);
+
+        return total - 3 * t1 + 3 * t2 - t3;
     }
 };
