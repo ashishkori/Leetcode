4fc2bed898bf9afc61a66e7bde1c5d99ddb945ad
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-07-19 22:39:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (78.32%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -14,6 +14,6 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==nullptr && q==nullptr) return true;
         if(p==nullptr || q==nullptr) return false;
-        return (p->val == q->val && isSameTree(p->left,q->left)&&isSameTree(p->right,q->right));
+        return ((p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
     }
 };
