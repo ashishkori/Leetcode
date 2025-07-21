bc28efdbe82f185cfc38858f33b9546490f60b8f
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-07-21 18:25:01 +0530

    Time: 35 ms (33.73%) | Memory: 44.4 MB (67.47%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -5,7 +5,6 @@ public:
         if (s.length() <= 2) {
             return s;
         }
-        
         // Result string to build our fancy string
         string result = s.substr(0,2);
         int j=result.size()-1;
@@ -13,20 +12,12 @@ public:
         for (int i = 2; i < s.length(); i++) {
             // Get the current character
             char currentChar = s[i];
-            
-            // Check if adding this character would create three consecutive identical characters
-            // We need at least 2 characters in result to make this check
-            
             if(result[j] == result[j-1] && result[j] == currentChar) {
-                // Skip this character because it would create three consecutive identical characters
                 continue;
             }
-            
-            // Safe to add this character - it won't create three consecutive identical characters
             result += currentChar;
             j++;
         }
-        
         return result;
     }
 };
