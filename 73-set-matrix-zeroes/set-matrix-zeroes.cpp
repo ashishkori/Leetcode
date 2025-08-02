930f12830628c9dd872cd9946e2e28d001a05788
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-08-02 21:39:20 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (51.65%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -18,12 +18,12 @@ public:
     for(int i=1;i<row;i++) {
         for(int j=1;j<col;j++) {
             if(matrix[i][j]) {
-                if(matrix[i][0] == 0 || matrix[0][j] ==0 ) matrix[i][j] = 0;
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
     }
     if(matrix[0][0]==0) {
-        for(int i=0;i<col; i++) {
+        for(int i=0; i<col; i++) {
             matrix[0][i] = 0;
         }
     }
