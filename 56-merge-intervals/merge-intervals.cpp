1eb2be1b7765cc3bef8b9c20d0fd166a8086f22d
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-08-02 23:10:02 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (57.20%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,14 +5,12 @@ public:
          sort(intervals.begin(),intervals.end());
          result.push_back(intervals[0]);
          for(int i=1;i<intervals.size();i++){
-            vector<int> curr=intervals[i];
-            int last=result.back()[1];
-            if(last>=curr[0]) {
-               // int start = result.back()[0];
-                //result.pop_back();
-                result.back()[1]=max(curr[1],last);
+           // vector<int> curr=intervals[i];
+            //int last=result.back()[1];
+            if(result.back()[1]>=intervals[i][0]) {
+                result.back()[1]=max(intervals[i][1],result.back()[1]);
             } else {
-                result.push_back(curr);
+                result.push_back(intervals[i]);
             }
          }
          return result;
