d1aa06d61963abf45947aebe0d0634082a7ce092
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-08-03 09:58:03 +0530

    Time: 0 ms (100.00%) | Memory: 132.1 MB (14.79%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,16 +11,16 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int>st;
-        ListNode *slow=head, *fast=head;
+        vector<int> nums;
+        ListNode *curr=head;
         if(!head->next) return true;
-        while(slow) {
-            st.push_back(slow->val);
-            slow=slow->next;
+        while(curr) {
+            nums.push_back(curr->val);
+            curr=curr->next;
         }
-        int l=0, r=st.size()-1;
+        int l=0, r=nums.size()-1;
         while(l<r) {
-            if(st[l++]!=st[r--]) return false;
+            if(nums[l++]!=nums[r--]) return false;
         }
         return true;
     }
