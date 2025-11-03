ab2a27af749a08c7758b2585f2a62e6ef9fd2540
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-11-03 18:57:22 +0530

    Time: 0 ms (100.00%) | Memory: 99.9 MB (35.11%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int minCost(string colors, vector<int>& neededTime) {
-        int n = colors.size(), sum = 0;
+    int minCost(string col, vector<int>& time) {
+        int n = col.size(), sum = 0;
         
         for (int i = 1; i < n; i++) {
-            int maxi = 0; // largest value balloon in substring
-            while (i < n and colors[i] == colors[i - 1]) {
-                sum += neededTime[i - 1];
-                maxi = max(maxi, neededTime[i - 1]);
-                ++i;
+            if (col[i] == col[i - 1]) {
+                sum += min(time[i], time[i - 1]);
+                time[i] = max(time[i], time[i - 1]);
             }
-            sum += neededTime[i - 1];
-            maxi = max(maxi, neededTime[i - 1]);
-            if (maxi != 0) sum -= maxi;
         }
         return sum;
     }
