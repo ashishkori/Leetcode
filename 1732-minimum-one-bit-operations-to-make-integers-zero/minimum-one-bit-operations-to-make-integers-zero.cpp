a4d713050018c85e18c0796b1abacb7f6403824e
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-22 11:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (45.01%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
-        int res = 0;
-        while (n) {
-            res ^= n;
-            n >>= 1;
+        vector<long> F(32,0);
+        F[0]=1;
+        for(int i=1;i<32;i++) {
+            F[i]=2*F[i-1]+1;
         }
-        return res;
+        int sign=1;
+        long res=0;
+        for(int i=31;i>=0;i--) {
+            if((n&(1<<i))) {
+                res+=(sign * F[i]);
+                sign*=-1;
+            }
+         }
+         return res;
     }
 };
