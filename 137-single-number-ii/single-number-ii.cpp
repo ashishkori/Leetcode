14e1099118965d65511172631069ecb71889ad65
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-22 08:47:40 +0530

    Time: 1 ms (42.10%) | Memory: 13.4 MB (65.73%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int res=0;
         int c1=0,c0=0;
-        for(int i=0;i<32;i++) {
+        for(int i=0;i<(sizeof(int)*8);i++) {
             int c1=0,c0=0;
             for(auto x:nums) {
                 if(x&(1<<i)) {
