4ee7bae534e31d69a052e4974f591a22f95416c2
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-22 15:41:57 +0530

    Time: 2 ms (35.00%) | Memory: 11.3 MB (12.51%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
    
+    // int rangeBitwiseAnd(int left, int right) {
+    //     int i=0;
+    //     while(left !=right) {
+    //         i++;
+    //         left>>=1;
+    //         right>>=1;
+    //     }
+    //     return left<<i;
+    // }
+//second approch
     int rangeBitwiseAnd(int left, int right) {
-        int i=0;
-        while(left !=right) {
-            i++;
-            left>>=1;
-            right>>=1;
+        while(right>left) {
+            right&=(right-1);
         }
-        return left<<i;
+        return right;
     }
 };
