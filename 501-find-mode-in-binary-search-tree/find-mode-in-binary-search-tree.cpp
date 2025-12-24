d6699123f671095cf6884ee486daa451beba7b60
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-24 11:19:45 +0530

    Time: 8 ms (8.36%) | Memory: 25.4 MB (64.25%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -12,13 +12,17 @@
 class Solution {
 public:
     map<int,int>mp;
+    vector<int> result;
+    int maxF=0;
+    int currE=0;
+    int currF=0;
     void dfs(TreeNode* root) {
         if(!root) return;
         dfs(root->left);
         mp[root->val]++;
         dfs(root->right);
     }
-    vector<int> findMode(TreeNode* root) {
+    vector<int> findMode1(TreeNode* root) {
         dfs(root);
         int maxF=0;
         vector<int> res;
@@ -32,5 +36,30 @@ public:
             }
         }
         return res;
+    }
+    void dfs2(TreeNode* root) {
+        if(!root) return;
+        dfs2(root->left);
+        if(root->val == currE) {
+             currF++;
+        } else {
+            currE=root->val;
+            currF=1;
+        }
+        
+        if(currF>maxF) {
+            maxF=currF;
+            result={currE};
+        }else if(currF==maxF) result.push_back(currE);
+
+        dfs2(root->right);
+    }
+    vector<int> findMode2(TreeNode* root) { 
+        dfs2(root);
+
+        return result;
+    }
+    vector<int> findMode(TreeNode* root) {
+        return 0?findMode1(root):findMode2(root);
     }
 };
