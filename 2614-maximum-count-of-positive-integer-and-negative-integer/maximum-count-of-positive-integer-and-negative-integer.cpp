d6d4533d06d4606bedc6b83aeba1d970fd6ab786
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-24 08:34:55 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (61.62%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -11,11 +11,33 @@ public:
         int c1 = count_if(nums.begin(),nums.end(),lambdaN);
         return max(c,c1);
         }
+        int myLowerBound(vector<int>& nums, int n){
+            int l=0;
+            int res=-1;
+            int r=nums.size()-1;
+            while(l<=r){
+                int mid = l+(r-l)/2;
+                if(nums[mid]>=n) {
+                    r=mid-1;
+                    res=mid;  
+                } else {
+                     l=mid+1;
+                }
+            }
+            return l;
+        }
         int maximumCount2(vector<int>& nums) {
             int n=nums.size();
-            int po = nums.end()- lower_bound(nums.begin(),nums.end(),1);
-            int ne = lower_bound(nums.begin(),nums.end(),0)-nums.begin();
-            return max(po,ne);
+            // int po = nums.end()- lower_bound(nums.begin(),nums.end(),1);
+            // int ne = lower_bound(nums.begin(),nums.end(),0)-nums.begin();
+
+            //return max(po,ne);
+            int pos=myLowerBound(nums,1);
+            int neg=myLowerBound(nums,0);
+            cout<< pos <<" "<<neg;
+            return max(n-pos,neg);
+
+
         }
         int maximumCount(vector<int>& nums) { 
             return 0?maximumCount1(nums):maximumCount2(nums);
