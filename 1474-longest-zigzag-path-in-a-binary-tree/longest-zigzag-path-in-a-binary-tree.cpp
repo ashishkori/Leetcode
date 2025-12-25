8f5154e0876ec3547ca657086b0c2b4b1b605ee3
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-25 23:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 94.5 MB (82.90%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -23,8 +23,27 @@ public:
             solve(root->left,1,false);
         }
     }
-    int longestZigZag(TreeNode* root) {
+
+    int longestZigZag1(TreeNode* root) {
         solve(root,0,true);
+        solve(root,0,false);
         return res;
+    }
+    void solve2(TreeNode* root, int l, int r) { 
+        if(!root) return;
+        res=max(res,l);
+        res=max(res,r);
+        solve2(root->left,r+1,0);
+        solve2(root->right,0,l+1);
+
+    }
+    int longestZigZag2(TreeNode* root) {
+        solve2(root,0,0);
+        return res;
+    }
+
+    int longestZigZag(TreeNode* root) {
+        
+        return 0?longestZigZag1(root):longestZigZag2(root);
     }
 };
