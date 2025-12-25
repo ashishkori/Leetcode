f1e8a30456c7cc4a77f038c7287d7fda2dc5c4ff
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-25 12:15:42 +0530

    Time: 4 ms (2.70%) | Memory: 15 MB (63.24%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,22 +11,42 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
+    vector<int> rightSideView1(TreeNode* root) {
         queue<TreeNode*> qu;
         vector<int> res;
         qu.push(root);
         if(!root) return {};
         while(!qu.empty()) {
             int s=qu.size();
-            while(s>0) {
-                TreeNode* curr = qu.front();
+             TreeNode* curr = NULL;
+            while(s) {
+                curr = qu.front();
                 qu.pop();
-                if(s==1) res.push_back(curr->val);
+                //if(s==1) res.push_back(curr->val);
                 if(curr->left) qu.push(curr->left);
                 if(curr->right) qu.push(curr->right);
                 s--; 
             }
+            res.push_back(curr->val);
         }
         return res;
+    }
+    void dfs(TreeNode* root, int level,  vector<int>& res) {
+        if(!root) return;
+        if(res.size()<level) {
+            res.push_back(root->val);
+        }
+        dfs(root->right,level+1,res);
+        dfs(root->left,level+1,res);
+
+    }
+    vector<int> rightSideView2(TreeNode* root) {
+        vector<int> res;
+        dfs(root, 1,res);
+        return res;
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+       return 0?rightSideView1(root):rightSideView2(root);
     }
 };
