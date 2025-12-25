ff925c379755f261abef5c886f36e638bb734926
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-25 09:22:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (10.91%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int n;
+    int dp[201][1001];
+    int solve(int idx, vector<int>& nums, int target) {
+        if(target == 0) {
+            return 1;
+        }
+        if(idx>=n || target<0) return 0;
+        if(dp[idx][target]!=-1) return dp[idx][target];
+        int take = solve(0,nums,target-nums[idx]);
+        int rej  = solve(idx+1,nums,target);
+        return dp[idx][target]=take+rej;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        n=nums.size();
+        int count=0;
+        memset(dp,-1,sizeof(dp));
+
+        return solve(0,nums,target);
+        //return count;
+        
+    }
+};
