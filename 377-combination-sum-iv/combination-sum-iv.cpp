b1d9c1096053087ff8f5bceb4ebe3194248046d2
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-25 09:27:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (12.09%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -12,12 +12,25 @@ public:
         int rej  = solve(idx+1,nums,target);
         return dp[idx][target]=take+rej;
     }
+
+     int solve1(int idx, vector<int>& nums, int target) {
+        if(target == 0) {
+            return 1;
+        }
+        if(idx>=n || target<0) return 0;
+        if(dp[idx][target]!=-1) return dp[idx][target];
+        int count=0;
+        for(int i=idx;i<n;i++) {
+            count += solve1(0,nums,target-nums[i]);
+        }
+        return dp[idx][target]=count;
+    }
     int combinationSum4(vector<int>& nums, int target) {
         n=nums.size();
         int count=0;
         memset(dp,-1,sizeof(dp));
 
-        return solve(0,nums,target);
+        return 0?solve(0,nums,target):solve1(0,nums,target);
         //return count;
         
     }
