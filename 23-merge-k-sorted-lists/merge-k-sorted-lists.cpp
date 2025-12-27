616d343b0c19926195dbc0d9fddf554ad00f1693
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-27 23:08:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (15.22%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,29 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) {
-            return nullptr;
-        }
-        return mergeKListsHelper(lists, 0, lists.size() - 1);
-    }
-    
-    ListNode* mergeKListsHelper(vector<ListNode*>& lists, int start, int end) {
-        if (start == end) {
-            return lists[start];
-        }
-        if (start + 1 == end) {
-            return merge(lists[start], lists[end]);
-        }
-        int mid = start + (end - start) / 2;
-        ListNode* left = mergeKListsHelper(lists, start, mid);
-        ListNode* right = mergeKListsHelper(lists, mid + 1, end);
-        return merge(left, right);
-    }
-    
-    ListNode* merge(ListNode* l1, ListNode* l2) {
+    ListNode * merge2Lists(ListNode* l1, ListNode* l2) {
         ListNode* dummy = new ListNode(0);
         ListNode* curr = dummy;
-        
+  
         while (l1 && l2) {
             if (l1->val < l2->val) {
                 curr->next = l1;
@@ -38,5 +28,23 @@ public:
         curr->next = l1 ? l1 : l2;
         
         return dummy->next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        queue<ListNode*> qu;
+        for(auto x:lists) { 
+            if(x) qu.push(x); 
+        }
+
+        while(qu.size()>1){
+            ListNode* l1=qu.front();
+             qu.pop();     
+            ListNode* l2=qu.front();
+             qu.pop(); 
+            ListNode* newl=merge2Lists(l1,l2);
+            qu.push(newl);
+        }
+
+        return   qu.empty() ? nullptr :qu.front();
     }
 };
