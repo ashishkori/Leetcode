42c66e5ce5566c1e75b5413dabf9636982f3f7c4
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-28 23:11:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (36.64%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -10,21 +10,37 @@
  */
 class Solution {
 public:
+    // ListNode * reverse(ListNode* head) {
+    //     if(!head) return head;
+
+    //     ListNode* curr=head;
+    //     ListNode* prev=nullptr;
+    //     ListNode* next=nullptr;
+    //     while(curr) {
+    //         next=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=next;
+    //     }
+
+    //     return prev;
+    // }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if (!head || left == right) return head;
-        ListNode* dummy = new ListNode(0);
+        if(!head || !head->next) return head;
+        ListNode* dummy=new ListNode(0);
         dummy->next = head;
-        ListNode* prev = dummy;
-        for (int i = 1; i < left; ++i) {
-            prev = prev->next;
+        ListNode* prev=dummy;
+        for(int i=1;i<left;i++) {
+            prev=prev->next;
         }
-        ListNode* curr = prev->next;
-        for (int i = 0; i < right - left; ++i) {
-            ListNode* temp = curr->next;
-            curr->next = temp->next;
-            temp->next = prev->next;
-            prev->next = temp;
-        }
+         ListNode* curr=prev->next;
+         for(int i=1;i<=right-left;i++) {
+                 ListNode* tmp=prev->next;
+                 prev->next=curr->next;
+                 curr->next=curr->next->next;
+                 prev->next->next=tmp;
+         }
         return dummy->next;
+
     }
 };
