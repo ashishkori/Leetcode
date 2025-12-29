747348c86d1fa41ea755ad5fa04b2315c592dc3a
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-29 22:08:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (10.01%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maximum69Number (int num) {
+    int maximum69NumberS (int num) {
         string s=to_string(num);
         for(int i=0;i<s.size();i++) {
             if(s[i]=='6') {
@@ -9,5 +9,26 @@ public:
             }
         }
         return stoi(s);
+    }
+    int maximum69NumberN (int num) { 
+        int n=num;
+        int pl=0;
+        int pls=-1;
+
+        while(n) {
+            if(n%10==6) {
+                pls=pl;
+            }
+            n/=10;
+            pl++;
+        }
+        if(pls==-1) return num;
+
+        num=num+(3*pow(10,pls));
+        return num;
+
+    }
+    int maximum69Number (int num) {
+        return 0?maximum69NumberS(num):maximum69NumberN(num);
     }
 };
