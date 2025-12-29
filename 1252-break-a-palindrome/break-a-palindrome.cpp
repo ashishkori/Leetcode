123f435330e685b5e0c44c8570d1c775dcbd1271
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-29 19:42:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (70.51%) - LeetSync

--- 1252-break-a-palindrome/break-a-palindrome.cpp
+++ 1252-break-a-palindrome/break-a-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string breakPalindrome(string s) {
         int n=s.size();
-        if(n<2) return "";
+        if(n==1) return "";
 
         for(int i=0;i<n/2;i++) {
             if(s[i]!='a') {
