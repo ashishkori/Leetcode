c7a61cea867d3fcd52173aab6edf1214b744590e
ashishkori <42586036+ashishkori@users.noreply.github.com>
2025-12-29 19:41:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (70.51%) - LeetSync

---
+++ 1252-break-a-palindrome/break-a-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        int n=s.size();
+        if(n<2) return "";
+
+        for(int i=0;i<n/2;i++) {
+            if(s[i]!='a') {
+                s[i]='a';
+                return s;
+            }
+        }
+        s[n-1]='b';
+        return s;
+    }
+};
