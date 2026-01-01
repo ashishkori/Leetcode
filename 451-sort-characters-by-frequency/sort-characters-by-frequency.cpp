be8cfd9cf5e5b2ddd5039f2e88b377aee1a01a90
ashishkori <42586036+ashishkori@users.noreply.github.com>
2026-01-01 21:28:34 +0530

    Time: 4 ms (36.66%) | Memory: 11.2 MB (54.15%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,21 +1,52 @@
 class Solution {
 public:
-    string frequencySort(string s) {
-        unordered_map<char, int> freq;
-        for(char ch : s){
-            freq[ch]++;
+    typedef pair<int,int> P;
+    struct lambda {
+        bool operator()(P& p1, P& p2) {
+            return p1.second<p2.second;
+        };
+    };
+    string frequencySort2(string s) {
+        unordered_map<char,int> mp;
+        priority_queue<P, vector<P>,lambda> pq;
+        for(int i=0;i<s.size();i++) {
+            mp[s[i]]++;
         }
-        vector<pair<char, int>> freqVec(freq.begin(), freq.end());
-
-        sort(freqVec.begin(), freqVec.end(), [](auto &a, auto &b) {
-            return a.second > b.second; 
-        });
-
-        string result;
-        for (auto &[ch, count] : freqVec) {
-            result.append(count, ch); 
+  
+        for(auto [x,c]:mp) {
+            pq.push({x,c});
         }
-
-        return result;
+        string res="";
+        while(!pq.empty()) {
+            char x=pq.top().first;
+            int count=pq.top().second;
+            pq.pop();
+            while(count>0) {
+                res+=x;
+                count--;
+            }
+        }
+        return res;
+    }
+    static bool compare(pair<char,int> &p1, pair<char,int>&p2) {
+        return  (p1.second > p2.second) ;
+    }
+    string frequencySort1(string s) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++) {
+            mp[s[i]]++;
+        }
+        vector<pair<char,int>> vec(mp.begin(),mp.end());
+        sort(vec.begin(),vec.end(),compare);
+        string res="";
+        for(auto x:vec) {
+            for(int i=0;i<x.second;i++) {
+                res+=x.first;
+            }
+        }
+        return res;
+    }
+    string frequencySort(string s)  {
+        return 0?frequencySort1(s):frequencySort2(s);
     }
 };
