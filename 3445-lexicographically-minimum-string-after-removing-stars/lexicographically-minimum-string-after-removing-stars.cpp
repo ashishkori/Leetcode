429e9bffdff724729e4b452290c4a7992d701d39
ashishkori <42586036+ashishkori@users.noreply.github.com>
2026-01-02 21:32:49 +0530

    Time: 133 ms (59.23%) | Memory: 26 MB (69.23%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    typedef pair<char,int> P;
+    struct lambda{
+        bool operator()(P& p1, P&p2){
+            if(p1.first==p2.first) return p1.second<p2.second;
+            return p1.first>p2.first;
+        }
+    };
+    string clearStars(string s) {
+        priority_queue<P,vector<P>,lambda> pq;
+        int n=s.size();
+        for(int i=0;i<n;i++) {
+            if(s[i]!='*') pq.push({s[i],i});
+            else{
+                char ch=pq.top().first;
+                int idx=pq.top().second;
+                pq.pop();
+                s[idx]='*';
+            }
+        }
+        string res="";
+        for(int i=0;i<n;i++) {
+            if(s[i]!='*') res.push_back(s[i]);
+        }
+        return res;
+    }
+};
