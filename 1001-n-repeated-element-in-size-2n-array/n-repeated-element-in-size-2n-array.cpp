0299ceea483fd0438ead661b64c883b9ab213017
ashishkori <42586036+ashishkori@users.noreply.github.com>
2026-01-02 09:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (69.74%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    int repeatedNTimes(vector<int>& nums) {
-        unordered_set<int> st;
-        for(auto &x:nums) {
-            if(st.find(x)!=st.end()) return x;
-            st.insert(x);
-        }
-        return -1;
+    int repeatedNTimes(vector<int>& A) {
+        for (int i = 0; i < A.size() - 1; ++i)
+            if (A[i] == A[i + 1] || (i + 2 < A.size() && A[i] == A[i + 2]))
+                return A[i];
+        
+        return A[0];
     }
 };
