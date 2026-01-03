4573852440e84992427662c8486a5cfdbce04f83
ashishkori <42586036+ashishkori@users.noreply.github.com>
2026-01-03 19:53:33 +0530

    Time: 135 ms (26.24%) | Memory: 224.5 MB (22.70%) - LeetSync

--- 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    static int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
-        const int n=nums.size(), qz=queries.size();
-        vector<vector<int>> qEnd(n);
-        for(int i=0; i<qz; i++)
-            qEnd[queries[i][0]].push_back(queries[i][1]);
-
-        priority_queue<int> pq;// max heap for ends of queries
-        // number of overlapping queries ending at i in line sweeping
-        vector<int> cntQ(n+1, 0);
-        int dec=0;
-        for(int i=0, j=0; i<n; i++){
-            const int x=nums[i];
-            dec+=cntQ[i];// current max possible number for decrementing
-            for(int j: qEnd[i])
-                pq.push(j);// push ends of queries starting at i
-            int k;
-            for(; x>dec && pq.size()>0 && (k=pq.top())>=i; dec++){
-                cntQ[k+1]--;
-                pq.pop();
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        priority_queue<int> pq;
+        priority_queue<int, vector<int>, greater<int>> past;
+        int Q=queries.size();
+        int usedQ=0;
+        sort(queries.begin(),queries.end());
+        int j=0;
+        for(int i=0;i<n;i++) {
+            while(j<Q && queries[j][0]==i) {
+                pq.push(queries[j][1]);
+                j++;
             }
-            if (x>dec) return -1;
+
+            nums[i]-=past.size();
+            while(nums[i]>0 && !pq.empty() && pq.top()>=i){
+                int x=pq.top();
+                pq.pop();
+                nums[i]--;
+                past.push(x);
+                usedQ++;
+            }
+
+            if(nums[i]>0) return -1;
+
+            while(!past.empty() && past.top()<=i) past.pop();
         }
-        return pq.size();
+        return Q-usedQ;
+
     }
 };
