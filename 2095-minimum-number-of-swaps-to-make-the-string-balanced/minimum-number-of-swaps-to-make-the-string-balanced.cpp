4911d42f2640e83016ccc7bd13031a2b1a77b114
ashishkori <42586036+ashishkori@users.noreply.github.com>
2026-01-05 10:54:23 +0530

    Time: 19 ms (30.83%) | Memory: 42 MB (5.28%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
-    int minSwaps(string s) {
+int minSwaps3(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++) {
+            if(!st.empty() && st.top()=='[' && s[i]==']') st.pop();
+            else st.push(s[i]);
+        }
+        // int count=0;
+        // while(!st.empty()) {
+        //     if(st.top()==']') count++;
+        //     st.pop();
+        // }
+        return (st.size()+1)/2;
+    }
+
+    int minSwaps2(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++) {
+            if(s[i]=='[') st.push(s[i]);
+            else if (!st.empty()) st.pop();
+            else st.push(s[i]);
+        }
+        
+        return (st.size()+1)/2;
+    }
+    int minSwaps1(string s) {
         stack<char> st;
         for(int i=0;i<s.size();i++) {
             if(!st.empty() && st.top()=='[' && s[i]==']') st.pop();
@@ -12,5 +36,8 @@ public:
             st.pop();
         }
         return (count+1)/2;
+    }
+    int minSwaps(string s) { 
+        return 0?minSwaps1(s):minSwaps2(s);
     }
 };
