7b41a210d5d1f64a9d9977563cd2ed84889b00e9
ashishkori <42586036+ashishkori@users.noreply.github.com>
2026-01-05 10:36:50 +0530

    Time: 32 ms (43.05%) | Memory: 29.4 MB (31.62%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
-    int minimumDeletions(string s) {
+     int minimumDeletions2(string s) { 
+        int count_a=0;
+        for(int i=0;i<s.size();i++) {
+            if(s[i]=='a') count_a++;
+        }
+        int count_b=0;
+        int res=s.size();
+        for(int i=0;i<s.size();i++) {
+            if(s[i]=='a') count_a--;
+        
+            res=min(res, (count_a+count_b));
+            if(s[i]=='b') count_b++;
+        }
+        return res;
+     }
+    int minimumDeletions1(string s) {
         stack<char> st;
         int count=0;
         for(int i=0;i<s.size();i++){
@@ -12,5 +27,8 @@ public:
             }
         }
         return count;
+    }
+    int minimumDeletions(string s) { 
+        return 0?minimumDeletions1(s):minimumDeletions2(s);
     }
 };
