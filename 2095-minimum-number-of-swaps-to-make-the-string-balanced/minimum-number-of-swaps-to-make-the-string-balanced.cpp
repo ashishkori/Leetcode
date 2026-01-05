6372c4f94d0194a14352ea41cc978946cc1a0dea
ashishkori <42586036+ashishkori@users.noreply.github.com>
2026-01-05 10:57:36 +0530

    Time: 18 ms (31.77%) | Memory: 39.5 MB (6.10%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
 int minSwaps3(string s) {
-        stack<char> st;
+        int size=0;
         for(int i=0;i<s.size();i++) {
-            if(!st.empty() && st.top()=='[' && s[i]==']') st.pop();
-            else st.push(s[i]);
+            if(s[i]=='[') size++;
+            else if (size>0) size--;
         }
-        // int count=0;
-        // while(!st.empty()) {
-        //     if(st.top()==']') count++;
-        //     st.pop();
-        // }
-        return (st.size()+1)/2;
+        
+        return (size+1)/2;
     }
 
     int minSwaps2(string s) {
@@ -38,6 +34,6 @@ int minSwaps3(string s) {
         return (count+1)/2;
     }
     int minSwaps(string s) { 
-        return 0?minSwaps1(s):minSwaps2(s);
+        return 0?minSwaps1(s):(0?minSwaps2(s):minSwaps3(s));
     }
 };
