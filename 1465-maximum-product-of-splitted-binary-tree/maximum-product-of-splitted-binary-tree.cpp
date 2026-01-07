d2f4dd4b041b06323e6a71974c67fe88effc2599
ashishkori <42586036+ashishkori@users.noreply.github.com>
2026-01-07 08:51:53 +0530

    Time: 7 ms (33.33%) | Memory: 78.9 MB (32.16%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -1,28 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Variant with an extra compute_ans=0 in dfs
 class Solution {
 public:
     const int mod=1e9+7;
     long long ans=INT_MIN, total=0;
-    int dfs(TreeNode* root){
+    int dfs(TreeNode* root, bool compute_ans=0){
         if (!root) return 0;
-        int sum=root->val+dfs(root->left)+dfs(root->right);
-        ans=max(ans, (total-sum)*sum);
+        int sum=root->val
+            +dfs(root->left, compute_ans)
+            +dfs(root->right, compute_ans);
+        if (compute_ans) ans=max(ans, (total-sum)*sum);
         return sum;
     }
     int maxProduct(TreeNode* root) {
         total=dfs(root);
-        dfs(root);
-    //    cout<<total<<", "<<ans<<endl;
+        dfs(root, 1);
         return ans%mod;
     }
 };
